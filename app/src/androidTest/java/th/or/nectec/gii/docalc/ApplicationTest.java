e7b052924aeea2720f12bc76ee86be50007397fe
Mink <khongpan@gmail.com>
2016-03-05 09:29:12 +0700

    Initial commit

---
+++ app/src/androidTest/java/th/or/nectec/gii/docalc/ApplicationTest.java
@@ -0,0 +1,13 @@
+package th.or.nectec.gii.docalc;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
